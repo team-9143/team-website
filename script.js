4e5bc60f14d9f9ace507983abad3e20ccccc9c2b
Sid Thomas <thomas.siddharth@gmail.com>
2023-08-25 21:04:03 -0700

    [fix] Working slideshow, css needs to be fixed

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-//Slideshow
-let slideIndex = 1;
-showSlides(slideIndex);
+// Home page slideshow
+var slideIndex = 0;
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+showSlide(slideIndex);
+
+function changeSlide(n) {
+  showSlide(slideIndex + n);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function setSlide(n) {
+  showSlide(n);
 }
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+function showSlide(n) {
+  let slides = document.querySelectorAll("#slideshow-container .mySlides");
+  let dots = document.querySelectorAll("#slideshow-container .dot");
+
+  (n >= slides.length) ? slideIndex = 0 :
+      (n < 0) ? slideIndex = slides.length-1 :
+        slideIndex = n;
+
+  for (let slide of slides) {slide.style.display = "none"}
+  slides[slideIndex].style.display = "block";
+
+  for (let dot of dots) {dot.classList.remove("active")}
+  dots[slideIndex].classList.add("active");
 }
