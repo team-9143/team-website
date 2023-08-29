64e0c4b3efb6f1e628c57ac191d0f79377340483
Sid <thomas.siddharth@gmail.com>
2023-08-28 22:22:24 -0700

    Formatting and Stylesheet Abstraction (#7)
    * [style] Fix indentation and spacing in css files
    
    * [fix] Add navbar stylesheet to html pages
    
    * [style] Rename script.js to slideshow.js
    
    * [style] Indentation and tag order fix
    
    * [style] Indentation fix
    
    * [chore] Comment out calendar and thank you pages
    
    * [refactor] Remove unnecessary setSlide function
    
    * [refactor] Move slideshow.css to style tag
    
    * [feat] Remove global box-sizing property

--- slideshow.js
+++ slideshow.js
@@ -1,17 +1,11 @@
 // Home page slideshow
 var slideIndex = 0;
 
-showSlide(slideIndex);
-
 function changeSlide(n) {
-  showSlide(slideIndex + n);
+  setSlide(slideIndex + n);
 }
 
 function setSlide(n) {
-  showSlide(n);
-}
-
-function showSlide(n) {
   let slides = document.querySelectorAll("#slideshow-container .mySlides");
   let dots = document.querySelectorAll("#slideshow-container .dot");
 
